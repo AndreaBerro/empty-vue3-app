db1b6f3c4cbdec3c0a72f8e6ea0eb1e0e9695332
Berro Andrea <andrea.berro@etu.unice.fr>
2021-12-02 18:30:27 +0100

    oui

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Oui v-for="index in 200" :key="index"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Oui from "./components/Oui.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Oui
   }
 }
 </script>
---
+++ src/components/Oui.vue
@@ -0,0 +1,13 @@
+<template>
+  <img alt="Random" src="../assets/RandomBitmap.png">
+</template>
+
+<script>
+
+export default {
+  name: 'Oui'
+}
+</script>
+
+<style>
+</style>
